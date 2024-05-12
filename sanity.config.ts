cf86fe7bc100ad27cf7b061b9b103a2c9ba35fda
Sahil Thapa <sahil@Sahils-MacBook-Air.local>
2024-05-12 23:04:09 +0545

    Initial commit

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes/schemaTypes";
+
+export default defineConfig({
+  name: "default",
+  title: "hotel-management-system",
+
+  projectId: "zwd80dnn",
+  dataset: "production",
+  basePath: "/studio",
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
