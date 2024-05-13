1c8751d35fcb481d4ead1e5ac6cd0e216bc2187d
Sahil Thapa <sahil@Sahils-MacBook-Air.local>
2024-05-13 22:15:28 +0545

    auth setup complete

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import { SanityAdapter, SanityCredentials } from "next-auth-sanity";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/github";
+import sanityClient from "./sanity";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,14 +14,14 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    SanityCredentials(),
-    // requires client
+    SanityCredentials(sanityClient),
+    // requires clients
   ],
   session: {
     strategy: "jwt",
   },
-  adapter: SanityAdapter(),
-  // requires client
+  adapter: SanityAdapter(sanityClient),
+  // requires clients
   debug: process.env.NODE_ENV === "development",
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {},
--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -1,1 +1,11 @@
 import { createClient } from "next-sanity";
+
+const sanityClient = createClient({
+  projectId: "zwd80dnn",
+  dataset: process.env.NEXT_SANITY_PUBLIC_DATASET,
+  useCdn: process.env.NODE_ENV === "production",
+  token: process.env.SANITY_SECRET_TOKEN,
+  apiVersion: "2021-10-21",
+});
+
+export default sanityClient;
