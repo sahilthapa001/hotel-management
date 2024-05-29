25d462988e4471bb8ff9492446dd2cb92cf1a0c8
Sahil Thapa <sahil@Sahils-MacBook-Air.local>
2024-05-29 22:32:15 +0545

    integrating stripe

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,52 @@
+import { authOptions } from "@/libs/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-04-10",
+});
+
+type RequestData = {
+  checkinDate: string;
+  checkoutDate: string;
+  adults: number;
+  children: number;
+  numberOfDays: number;
+  hotelRoomSlug: string;
+};
+export async function POST(req: Request, res: Response) {
+  const {
+    checkinDate,
+    adults,
+    checkoutDate,
+    children,
+    hotelRoomSlug,
+    numberOfDays,
+  }: RequestData = await req.json();
+  if (
+    !checkinDate ||
+    !checkoutDate ||
+    !adults ||
+    !hotelRoomSlug ||
+    !numberOfDays
+  ) {
+    return new NextResponse("Please all fields are required ", { status: 400 });
+  }
+
+  const origin = req.headers.get("origin");
+
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new NextResponse("Authentication required ", { status: 400 });
+  }
+  const userId = session.user.id;
+  const formattedCheckoutDate = checkoutDate.split("T")[0];
+  const formattedCheckinDate = checkinDate.split("T")[0];
+
+  try {
+  } catch (error: any) {
+    console.log("Payment faile", error);
+    return new NextResponse(error, { status: 500 });
+  }
+}
